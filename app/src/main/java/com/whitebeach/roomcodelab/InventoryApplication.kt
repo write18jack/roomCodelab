764cf1e508a6593a34cf48c5e7c29f7926ef8e24
write18jack <write18jack@gmail.com>
2024-08-14 20:13:23 +0900

    task room finish

---
+++ app/src/main/java/com/whitebeach/roomcodelab/InventoryApplication.kt
@@ -0,0 +1,18 @@
+package com.whitebeach.roomcodelab
+
+import android.app.Application
+import com.example.inventory.data.AppContainer
+import com.example.inventory.data.AppDataContainer
+
+class InventoryApplication : Application() {
+
+    /**
+     * AppContainer instance used by the rest of classes to obtain dependencies
+     */
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = AppDataContainer(this)
+    }
+}
